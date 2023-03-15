2098d732c8d2b9e72e4fa5ef7bd8b895873701e0
anner <anner@fanruan.com>
2023-03-15 16:38:52 +0800

    chore: 重新部署结构，将每一个微服务组件放到一个工程

---
+++ common-string/src/main/java/com/anner/common/string/StringUtils.java
@@ -0,0 +1,11 @@
+package com.anner.common.string;
+
+/**
+ * Created by anner on 2023/3/15
+ */
+public class StringUtils {
+
+    public static boolean isEmpty(String s) {
+        return false;
+    }
+}
